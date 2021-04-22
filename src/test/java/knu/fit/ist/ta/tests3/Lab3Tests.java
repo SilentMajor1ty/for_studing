6cdd22add66b86224b7537134e6456e15050bc83
sereg <sereg@DESKTOP-ADPKKNJ>
2021-04-22 23:00:46 +0300

    Lab 3 completed

---
+++ src/test/java/knu/fit/ist/ta/tests3/Lab3Tests.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knu.fit.ist.ta.tests3;
+
+
+
+
+import knu.fit.ist.ta.lab3.Array;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ *
+ * @author sereg
+ */
+@SpringBootTest
+
+public class Lab3Tests {
+   @Test
+   void test() {
+       Array array = new Array();
+       int[] a={7,4,1,0,10};
+        assertEquals("" + 0,array.func(a));        
+        }
+}
+
